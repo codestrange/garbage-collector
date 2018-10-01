a69e6125a81ca67acdf9fca2cea3e3877ebd973c
leynier <leynier41@gmail.com>
2018-10-01 18:13:11 -0400

    Modificar el parser para que sea Dinamico

--- src/utils_file.c
+++ src/utils_file.c
@@ -2,13 +2,16 @@
 #include "utils_file.h"
 #include "utils_list.h"
 
-ArrayList parse_file(char *file_name) {
+int get_file(int file_name) {
     int file_descriptor = open(file_name, O_RDONLY, 0664);
+    return file_descriptor;
+}
+
+char *get_line(int file_descriptor) {
     int index = 0;
     int top = 100;
     char *line = malloc(top * sizeof(char));
     char buffer;
-    ArrayList arraylist = new_arraylist(10);
     while(read(file_descriptor, &buffer, 1)) {
         line[index++] = buffer;
         if(index >= top) {
@@ -16,13 +19,16 @@ ArrayList parse_file(char *file_name) {
             line = realloc(line, top * sizeof(char));
         }
         if(buffer == 10) {
-            List list = parse_line(line);
-            append_arraylist(&arraylist, list);
-            index = 0;
+            line[index - 1] = 0;
+            return line;
         }
     }
     line[index] = 0;
-    List list = parse_line(line);
-    append_arraylist(&arraylist, list);
-    return arraylist;
+    return line;
+}
+
+List get_garbages(int file_descriptor) {
+    char *line = get_file(file_descriptor);
+    List result = parse_line(line);
+    return result;
 }
--- src/utils_file.h
+++ src/utils_file.h
@@ -2,7 +2,11 @@
 #define UTILS_FILE
 #include "utils_list.h"
 
-ArrayList parse_file(char *file_name);
+int get_file(int file_name);
+
+char *get_line(int file_descriptor);
+
+List get_garbages(int file_descriptor);
 
 List parse_line(char *line);
 
