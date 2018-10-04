21a03ac612072b1cd0aae2efb697164e39a6c3e1
leynier <leynier41@gmail.com>
2018-10-04 18:48:09 -0400

    Implementar el emzamblado de las partes del juego

--- src/main.c
+++ src/main.c
@@ -1,5 +1,73 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include "logit_game.h"
+#include "movement.h"
+#include "utils_file.h"
+#include "utils_list.h"
+#include "utils_matrix.h"
 
-int main(int argc, const char *argv[]) {
-    
+void print_error(char* error) {
+    system("clear");
+    printf("Error: %s\n", error);
+}
+
+int main(int argc, const char **argv) {
+    switch (argc) {
+        case 1:
+            printf("Error: Tiene que introducir la dirección del archivo de basuras y la dirección de la libreria que contiene la función de movimiento.\n");
+            return 0;
+        case 2:
+            printf("Error: Tiene que introducir la dirección de la libreria que contiene la función de movimiento.\n");
+            return 0;
+    }
+    const char *fn_garbages = argv[1];
+    int fd = get_file(fn_garbages);
+    int turns = 20;
+    int frequency = 10;
+    int temp_frequency = 10;
+    char **matrix = get_matrix(15, 15);
+    int score = 0;
+    int points = 10;
+    for (int i = 0; i < 15; ++i)
+        for (int j = 0; j < 15; ++j)
+            matrix[i][j] = '.';
+    matrix[0][0] = 'X';
+    Position robot = new_position(0, 0);
+    print_matrix(15, 15, score, matrix);
+    while (turns--) {
+        char c;
+        bool isEOF = false, isOk = true;
+        if (temp_frequency-- == frequency) {
+            List garbages = get_garbages(fd, &isEOF, &isOk);
+            if (isEOF) {
+                print_error("El archivo de basura se termino antes que la cantidad de turnos.");
+                break;
+            } else if(!isOk) {
+                print_error("El archivo de basura esta mal construido.");
+                break;
+            } else if(garbages.size != 5) {
+                print_error("Tiene que haber 5 basuras por línea en el archivo de basuras.");
+                break;
+            }
+            int temp_score = set_garbages(matrix, 15, 15, &garbages, points, &isOk);
+            if (!isOk) {
+                print_error("La posición de una basura en el archivo de basuras está fuera de la matrix.");
+                break;
+            }
+            score += temp_score;
+        }
+        if (temp_frequency == 0)
+            temp_frequency = frequency;
+        int dir = fmd(matrix);
+        int temp_score = move_robot(matrix, 15, 15, &robot, dir, points, &isOk);
+        if (!isOk) {
+            print_error("El robot ha salido de la matrix.");
+            break;
+        }
+        score += temp_score;
+        print_matrix(15, 15, score, matrix);
+        scanf("%c", &c);
+    }
+    close(fd);
 }
